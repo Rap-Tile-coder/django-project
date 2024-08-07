7323325cf39c239ff2c203202c67e50173cc7303
Suraj <psuraj4327@gmail.com>
2024-08-07 16:57:14 +0530

    crud operation: delete operation, update operation and url jumping are executed

--- new_app/urls.py
+++ new_app/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from new_app import views
 
 urlpatterns = [
-    path('',views.homePage, name = 'home'),
-    path('index', views.index, name='index'),
+    path('test',views.homePage, name = 'home'),
+    path('', views.index, name='index'),
     path('dashboard', views.dashBoard, name="landingpage"),
     path('forms',views.furnitureData, name="forms"),
-    path('dataview', views.furnitureDataView, name = "dataview")
+    path('dataview', views.furnitureDataView, name = "dataview"),
+    path('del/<int:id>/', views.items_delete, name= 'del'),
+    path('update/<int:id>/',views.itemUpdate,name= 'update')
 ]
--- new_app/views.py
+++ new_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from new_app.forms import furnitureForm
 from new_app.models import Furniture
@@ -15,13 +15,14 @@ def index(request):
 def dashBoard(request):
     return render(request, 'dashboard.html')
 
-#CRUD OPERATION : create
+#CRUD OPERATION : create operation
 def furnitureData(request):
     form = furnitureForm()
     if request.method == 'POST':
         data = furnitureForm(request.POST)
         if data.is_valid():
             data.save()
+            return redirect('')
     return render(request, 'form.html', {
         'form': form
     })
@@ -29,3 +30,26 @@ def furnitureData(request):
 def furnitureDataView(request):
     data = Furniture.objects.all()
     return render(request, 'view_data.html', {'data': data})
+
+#delete operation
+
+def items_delete(request,id):
+    data = Furniture.objects.get(id= id)
+    data.delete()
+    return redirect("dataview")
+
+
+# update method
+
+def itemUpdate(request, id):
+    data =Furniture.objects.get(id= id)
+    form = furnitureForm(instance=data)
+    if request.method == 'POST':
+        data = furnitureForm(request.POST, instance=data)
+        if data.is_valid():
+            data.save()
+            return redirect('dataview')
+
+    return render(request, 'detailsupdate.html', {'form': form})
+
+
